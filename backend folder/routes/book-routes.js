599702955ef2793cfc4ee25e7490373fbe54d9a7
suvarna <somavanshisuvarna48@gmail.com>
2023-07-24 20:13:38 +0530

    creating Schema

--- backend folder/routes/book-routes.js
+++ backend folder/routes/book-routes.js
@@ -3,8 +3,12 @@ import Book from "../routes/book-routes"
 const router = express.Router();
 
 router.get("/",(req,res,next)=>{
+ let books;
 
+ try{
 
-
-
+   
+ }catch(err){
+    console.log(err)}
+  
 })
